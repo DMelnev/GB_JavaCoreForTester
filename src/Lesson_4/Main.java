e7d2f4d25c0b45602a76477c1447a43b9a31d1ee
DMITRIY <dvmau@mail.ru>
2022-03-05 10:57:53 +0300

    created lesson 4 main class

--- src/Lesson_4/Main.java
+++ src/Lesson_4/Main.java
@@ -6,8 +6,48 @@
  **/
 package Lesson_4;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+
+//        exercise one:
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("it");
+        list.add("is");
+        list.add("a");
+        list.add("good");
+        list.add("day");
+        list.add("this");
+        list.add("day");
+        list.add("is");
+        list.add("a");
+        list.add("nice");
+        list.add("gorges");
+        list.add("day");
+        list.add("expect");
+        list.add("us");
+        list.add("how");
+        list.add("nice");
+        list.add("is");
+        list.add("the");
+        list.add("fish");
+        list.add("good");
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : list) {
+            if (map.containsKey(word)) map.replace(word, (map.get(word) + 1));
+            else map.put(word, 1);
+        }
+
+        System.out.println(map);
+
+//        Exercise two:
+
+
 
     }
 }
