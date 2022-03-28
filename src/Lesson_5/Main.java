bcf3e96774bf09d540595a937ffdee8179e1d175
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-03-28 19:46:41 +0300

    .

--- src/Lesson_5/Main.java
+++ src/Lesson_5/Main.java
@@ -1,7 +1,20 @@
 package Lesson_5;
 
 public class Main {
+    private final static String FILE_NAME = "file.csv";
+    private final static String FILE_NAME_RESULT = "fileResult.csv";
+
+
     public static void main(String[] args) {
-        System.out.println("df");
+
+        AppData data = new AppData();
+        ParserCSV parser = new ParserCSV();
+        FileWorker.setCharSet(Encoding.UTF8);
+
+        String textFile  = FileWorker.loadFile(FILE_NAME);
+        System.out.println(textFile);
+
+        parser.stringToData(textFile);
+
     }
 }
