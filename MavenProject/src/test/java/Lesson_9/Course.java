d212b22591d75ea4c4ade45469abd5e821f9443f
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-04-11 21:25:07 +0300

    solved

--- MavenProject/src/test/java/Lesson_9/Course.java
+++ MavenProject/src/test/java/Lesson_9/Course.java
@@ -9,17 +9,6 @@ public class Course {
         this.name = name;
     }
 
-    public Course() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- MavenProject/src/test/java/Lesson_9/Student.java
+++ MavenProject/src/test/java/Lesson_9/Student.java
@@ -12,10 +12,6 @@ public class Student{
         this.courses = courses;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public List<Course> getAllCourses() {
         return courses;
     }
