5a37714d40c372672af6c5f7a6c3b068a7a6a0a2
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-04-11 19:57:48 +0300

    .

--- MavenProject/src/test/java/Lesson_9/Main.java
+++ MavenProject/src/test/java/Lesson_9/Main.java
@@ -1,7 +1,22 @@
 package Lesson_9;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("Ivan");
+        List<Student> group = new ArrayList<>();
+        List<Course> stream = new ArrayList<>();
+        stream.add(new Course("Biology"));
+        stream.add(new Course("Geography"));
+        stream.add(new Course("History"));
+        stream.add(new Course("Literature"));
+        stream.add(new Course("Chemistry"));
+        stream.add(new Course("Botany"));
+        stream.add(new Course("Physics"));
+        stream.add(new Course("Computer Science"));
+
+        group.add(new Student("Ivanov", {}));
+
     }
 }
